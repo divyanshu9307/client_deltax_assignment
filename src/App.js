36da0c3639de24787b4d06f2a25f0fffddabd108
divyanshu9307 <divyanshu9307@gmail.com>
2022-06-08 01:03:10 +0530

    added song component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Header2 from './components/Header2';
 import Song from './components/Song';
 import Newsong from './subpages/Newsong';
 import despacitoo from "./images/despacito.jpg";
+import arcadee from "./images/arcade.jpg"
+import aree from "./images/are you with me.jpg"
+import duskk from "./images/dusk till dawn.jpg"
+import nightt from "./images/night we met.jpg"
 import senoritaa from "./images/senorita.jpg"
 
 import './App.css';
@@ -31,8 +35,32 @@ const App = () => {
            <Song
             Artwork={senoritaa}
             song="senorita"
-            dateOfRelease="may 3,2018"
+            dateOfRelease="august 28,2018"
             Artist="Shawn Mendes"
+          />
+           <Song
+            Artwork={arcadee}
+            song="arcade"
+            dateOfRelease="july 7,2019"
+            Artist="Duncan Laurence"
+          />
+           <Song
+            Artwork={aree}
+            song="are you with me"
+            dateOfRelease="june 18,2018"
+            Artist="Lost Frequencies"
+          />
+           <Song
+            Artwork={duskk}
+            song="senorita"
+            dateOfRelease="may 3,2018"
+            Artist="zayn malik"
+          />
+           <Song
+            Artwork={nightt}
+            song="senorita"
+            dateOfRelease="may 25,2018"
+            Artist="Lord Huron"
           />
         </Route>
         <Route exact path="/newsong">
