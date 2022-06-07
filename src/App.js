02f6912f3608297bceb50d5641e01531ed022160
divyanshu9307 <divyanshu9307@gmail.com>
2022-06-07 19:04:56 +0530

    routing set

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 
 import header from './components/header';
+import header1 from './components/header1';
+import header2 from './components/header2';
+import song from './components/song';
+import newsong from './subpages/newsong';
+
 import './App.css';
 import {Switch,Route} from "react-router-dom" ;
 
 function App() {
   return (
     <switch>
-      <Route path="/">
-      <div className="App">
-     {/* <h1>Assignmnent</h1> */}
-     <header></header>
-    </div>
+      <Route exact  path="/">
+     <header/>
+    <header1/>
+    <header2/>
+    <song/>
+      </Route>
+      <Route exact path="newsong">
+        <newsong/>
       </Route>
      
     </switch>
