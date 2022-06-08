ac183b822ef11eb0af30a3bca3664ae23b9286c4
divyanshu9307 <divyanshu9307@gmail.com>
2022-06-08 17:40:32 +0530

    added add_artist as popup

--- src/subpages/newsong.js
+++ src/subpages/newsong.js
@@ -1,8 +1,18 @@
 
 import React, { useState, memo } from "react";
+import Popup from "../components/Popup";
 import Multiselect from "multiselect-react-dropdown";
+import { Link } from "react-router-dom/";
+
+
 
 const Newsong = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openPop = (event) => {
+    event.preventDefault();
+    setIsOpen(!isOpen);
+  };
  
   const [artist] = useState([
     "Shawn mendes",
@@ -14,7 +24,63 @@ const Newsong = () => {
   
     return (
       <>
-       
+      {isOpen && (
+        
+        <Popup
+          content={ 
+          <>
+        <form
+                class="popup-handle"
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  // console.log(data);
+                }}
+              >
+                <h1 style={{ marginLeft: "10px" }}>Add Artist</h1>
+                <div className="input-design">
+                  <div>
+                    <div className="name">Artist Name</div>
+                    <div className="name">date of Birth</div>
+                    <div className="name">Bio</div>
+                  </div>
+                  <div className="input-div">
+                    <div className="input">
+                      <input
+                        name="artistname"
+                      
+                        type="text"
+                    
+                      />
+                     
+                    </div>
+                    <div className="input">
+                      <input
+                        type="date"
+                        name="date"
+                       
+                      />
+                    
+                    </div>
+                    <div className="input">
+                      <textarea
+                        type="text"
+                        name="bio"
+                       
+                      ></textarea>
+                     
+                    </div>
+                    <div className="input">
+                    <button type="submit">Done</button>
+                      <button onClick={openPop}>Cancel</button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+              </>
+          }
+          handleClose={openPop}
+        />
+      )}
         <form
           className="form-design"
         
@@ -63,16 +129,19 @@ const Newsong = () => {
               />
                
   
-                <button  className="artist-button">
+                <button  onClick={openPop}  className="artist-button">
               
                   + Add Artist
                 </button>
               </div>
               <div className="input-button">
                 <button>Save</button>
+                
                 <div className="cancel-button">
+                <Link to="/" id="cancel-color">
+                  Cancel
+                </Link> 
                  
-                  <button>Cancel</button>
                 </div>
               </div>
             </div>
