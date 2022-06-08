64fc9a062004b132997c982659e6b4cd71eef2e5
divyanshu9307 <divyanshu9307@gmail.com>
2022-06-08 15:37:42 +0530

    added new song page

--- src/App.js
+++ src/App.js
@@ -24,18 +24,18 @@ const App = () => {
     
    <Switch>
       <Route exact path="/">
-        {/* <Header />
+        <Header />
         <Topsongs/>
-         <Header1/> */}
-        {/* <Header2/>  */}
+         <Header1/>
+   
       
-        {/* <Song
+        <Song
             Artwork={despacitoo}
             song="despacito"
             dateOfRelease="july 10,2019"
             Artist="Luis Fonsi"
-          /> */}
-           {/* <Song
+          />
+           <Song
             Artwork={senoritaa}
             song="senorita"
             dateOfRelease="august 28,2018"
@@ -64,7 +64,7 @@ const App = () => {
             song="senorita"
             dateOfRelease="may 25,2018"
             Artist="Lord Huron"
-          /> */}
+          />
           <ArtistTopPage />
         <div id="artistdesign">
         <Header2/> 
@@ -88,8 +88,24 @@ const App = () => {
             dob="9 December 1965"
             songslist="Arcade"
           />
+           <Artistpage
+            name="Lord Huron"
+            dob="14 June 1987"
+            songslist="night we met"
+          />
+          <Artistpage
+            name="Alan Walker"
+            dob="19 july 1990"
+            songslist="On My Way"
+          />
+          <Artistpage
+            name="Duncan Laurence"
+            dob="9 December 1965"
+            songslist="Arcade"
+          />
         </div>
         </Route>
+        <Newsong/>
         <Route exact path="/newsong">
         
    
--- src/subpages/newsong.js
+++ src/subpages/newsong.js
@@ -1,9 +1,78 @@
-import React from 'react'
+
+import React, { useState, memo } from "react";
+import Multiselect from "multiselect-react-dropdown";
 
 const newsong = () => {
-  return (
-    <div>newsong</div>
+ 
+  
+    return (
+      <>
+       
+        <form
+          className="form-design"
+        
+          onSubmit={(event) => {
+            event.preventDefault();
+            
+          }}
+        >
+          <h1 style={{ marginLeft: "10px" }}>Adding a new Song</h1>
+          <div className="input-design">
+            <div>
+              <div className="name">Song Name</div>
+              <div className="name">Date Released</div>
+              <div className="name">Artwork</div>
+              <div className="name">Artist</div>
+            </div>
+            <div className="input-div">
+              <div className="input">
+                <input
+                  type="text"
+                  name="songname"
+                  />
+              </div>
+              <div className="input">
+                <input
+                  type="date"
+                  name="datereleased"
+                 />
+              </div>
+              <div className="input">
+                <input
+                  type="file"
+                  name="artwork"
+                 />
+              </div>
+              <div className="input" id="add-artist">
+                <Multiselect
+                  isObject={false}
+                  onRemove={(event)=>{console.log(event)}}
+                  onSelect={(event)=>{console.log(event)}}
+                 
+                  showCheckbox
+                 
+                   name="artists"
+                  
+                />
+               
+  
+                <button  className="artist-button">
+              
+                  + Add Artist
+                </button>
+              </div>
+              <div className="input-button">
+                <button>Save</button>
+                <div className="cancel-button">
+                 
+                  <button>Cancel</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </>
   )
 }
 
-export default newsong
+export default memo (newsong)
